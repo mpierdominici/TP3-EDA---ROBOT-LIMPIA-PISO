abc547487fb1bc0d1f6f6907b6ef1ca0ef9e8c78
Matias <mpierdominici@itba.edu.ar>
2017-03-24 16:57:36 -0300

    Create piso.cpp

---
+++ TP3 EDA LIMPIADOR DE PISO/TP3 EDA LIMPIADOR DE PISO/piso.cpp
@@ -0,0 +1,24 @@
+#include "piso.h"
+
+void piso::asignar_piso(unsigned int a, unsigned int l)
+{
+}
+
+bool piso::esta_sucio(bool & puntero_a_baldosa)
+{
+	return false;
+}
+
+void piso::limpiar_baldosa(unsigned int coord_x, unsigned int coord_y)
+{
+}
+
+bool piso::estado_de_la_baldosa(unsigned int coord_x, unsigned int coord_y)
+{
+	return false;
+}
+
+bool piso::asignacion_correcta()
+{
+	return false;
+}

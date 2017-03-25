5d8b7d7ab6119533c09e7ac5cfcd826c5394de84
Matias <mpierdominici@itba.edu.ar>
2017-03-25 17:10:59 -0300

    t

--- TP3 EDA LIMPIADOR DE PISO/TP3 EDA LIMPIADOR DE PISO/TP3 EDA LIMPIADOR DE PISO.cpp
+++ TP3 EDA LIMPIADOR DE PISO/TP3 EDA LIMPIADOR DE PISO/TP3 EDA LIMPIADOR DE PISO.cpp
@@ -2,10 +2,17 @@
 //
 
 #include "stdafx.h"
-
-
+#include <iostream>
+#include "graficar.h"
+#include "piso.h"
 int main()
 {
+	piso pi;
+	pi.asignar_piso(10, 10);
+	graficar p(10, 10,&pi);
+	p.GraficarMachine();
+
+
     return 0;
 }
 
